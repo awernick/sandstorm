b3f3cb211e347d4131297ffdb376b85391fce1e2
Alan Wernick <awernick@miners.utep.edu>
2015-01-12 16:00:24 -0700

    Enable Protothreading for Arduino communications

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,7 +5,7 @@
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-static const int cameraPin = 7;
+static const int cameraPin = 13;
 
 static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
@@ -126,6 +126,7 @@ void displaySensorInfo()
     Serial.println("Display Sensor Info");
     Serial.print("Snapshot...");
     digitalWrite(cameraPin, HIGH);
+    delay(1000);
     digitalWrite(cameraPin, LOW);
   }
 }
